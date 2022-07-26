1a24a298be7853051d839028a37cf8956e832621
islamhanii <islamhani433@gmail.com>
2022-07-27 01:37:56 +0200

    change informations structure

---
+++ database/migrations/2022_07_13_055737_create_informations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInformationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('informations', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['phone', 'email', 'opening-hours', 'address', 'facebook', 'youtube', 'twitter', 'instagram']);
+            $table->string('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('informations');
+    }
+}
